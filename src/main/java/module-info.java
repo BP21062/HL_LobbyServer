3ebf6a58b8e8f43a71ddd97456fe3b439693fd7d
watanabe mikio <bp21062@sibaura-it.ac.jp>
2023-12-13 01:54:23 +0900

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.hl_lobbyserver {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.hl_lobbyserver to javafx.fxml;
+    exports com.example.hl_lobbyserver;
+}
