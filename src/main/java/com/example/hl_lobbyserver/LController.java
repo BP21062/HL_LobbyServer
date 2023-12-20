5520e0a4afbf07161cfd1132966d177fcd3655c3
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2023-12-20 14:21:38 +0900

    クラスの新設

---
+++ src/main/java/com/example/hl_lobbyserver/LController.java
@@ -0,0 +1,40 @@
+package com.example.hl_lobbyserver;
+
+import java.util.ArrayList;
+
+public class LController{
+
+	public void register(String user_id, String password){
+		// TODO implement here
+	}
+
+	public Boolean login(String user_id, ArrayList<User> user_id_list){
+		// TODO implement here
+		return null;
+	}
+
+	public void login(String user_id, String password){
+		// TODO implement here
+	}
+
+	public Boolean verifyUserIDAndPassword(){
+		// TODO implement here
+		return null;
+	}
+
+	public void logout(String user_id, String password){
+		// TODO implement here
+	}
+
+	public String generateSuccessMessage(){
+		// TODO implement here
+		return "";
+	}
+
+	public String generateErrorMessage(){
+		// TODO implement here
+		return "";
+	}
+}
+
+
