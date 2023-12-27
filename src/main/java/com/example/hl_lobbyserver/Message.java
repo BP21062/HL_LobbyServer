57c5f49061048a84ccd59d4ad427603e7047115f
bp21009 <bp21009@shibaura-it.ac.jp>
2023-12-27 22:22:52 +0900

    MessageContentクラスの再調整

--- src/main/java/com/example/hl_lobbyserver/Message.java
+++ src/main/java/com/example/hl_lobbyserver/Message.java
@@ -1,7 +1,13 @@
 package com.example.hl_lobbyserver;
 
 public class Message{
-	String order;
-	MessageContent content;
-	Boolean result;
+	public MessageContent messageContent = new MessageContent();
+	String order;//通信規則用
+	Boolean result;//画面遷移とかのチェック用？
+
+	public Message(String order, String user_id){
+		this.order = order;
+		this.messageContent.user_id = user_id;
+	}
+
 }
