f22d902b84ddf45e443047b681722e0a6e1e10fc
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2024-01-10 16:58:37 +0900

    パッケージ周りの設定をIntellijに合わせる

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.hl_appserver {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javax.websocket.api;
+	requires com.google.gson;
+	requires tyrus.server;
+	requires java.ws.rs;
+	requires jersey.server;
+	requires grizzly.http.server;
+	requires jersey.container.grizzly2.http;
+	requires jersey.common;
+
+	opens com.example.hl_lobbyserver to javafx.fxml, com.google.gson;
+	exports com.example.hl_lobbyserver;
+}
